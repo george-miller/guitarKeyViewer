dbb3806f91d435ad9eaa539d9ffbecfbfb603fac
George Miller <geo.m.miller@gmail.com>
2016-09-22 22:34:10 -0600

    It works! Currently can show guitar with standard tuning in the key of A

--- index.php
+++ index.php
@@ -2,34 +2,44 @@
 <?php
 $error = '';
 
-function queryGetVariable($key, $defaultValue){
-    if ($_GET[$key]){
+function queryIntegerGetVariable($key, $defaultValue){
+    if (array_key_exists($key, $_GET)){
         return intval($_GET[$key]);
     } else {
         return $defaultValue;
     }
 }
 
-$rows = queryGetVariable('rows', 18);
-$columns = queryGetVariable('columns', 6);
-$startingValues = array();
-for ($i = 0; $i < $columns; $i++) {
-    array_push($startingValues, queryGetVariable('startingValue' . (string) $i, 1));
+function findCurrentScaleNote($startingValue, $notesInScale) {
+    for ($i = 0; $i < sizeof($notesInScale); $i++) {
+        if ($notesInScale[$i] >= $startingValue) {
+            return $i;
+        }
+    }
 }
 
-$keyType = 'major';
+$rows = queryIntegerGetVariable('rows', 18);
+$columns = queryIntegerGetVariable('columns', 6);
+
+// Values of the chromatic scale start at 1
+$startingValues = array(8, 1, 6, 11, 3, 8);
+for ($i = 0; $i < $columns; $i++) {
+    array_push($startingValues, queryIntegerGetVariable('startingValue' . (string) $i, $startingValues[$i]));
+}
+
+$keyType = array_key_exists('keyType', $_GET) ? $_GET['keyType'] : 'major';
 $chromaticScaleLength = 12;
-$stepLengths = null;
+$notesInScale = null;
 switch ($keyType){
 // If a note in the chromatic scale was one unit, 
 // these lengths would describe how to achive the
-// major scale from distances between notes. 
+// major scale from distances between notesInScale. 
 // Whole Whole Half Whole Whole Whole Half
     case 'major':
-        $stepLengths = [2, 2, 1, 2, 2, 2, 1];
+        $notesInScale = [1, 3, 5, 6, 8, 10, 12];
         break;
     case 'minor':
-        $stepLengths = [2, 1, 2, 2, 1, 2, 2];
+        $notesInScale = [1, 3, 4, 6, 8, 9, 11];
         break;
 }    
 
@@ -41,13 +51,64 @@ for ($i = 0; $i < $columns; $i++) {
     }
 }
 
-print_r($fretMatrix);
-for ($col = 0; $col < $columns; $columns++) {
-    array_push($fretMatrix, array());
+for ($col = 0; $col < $columns; $col++) {
+    $currentChromaticNote = $startingValues[$col];
+    $currentScaleNote = findCurrentScaleNote($currentChromaticNote, $notesInScale);
     for ($row = 0; $row < $rows; $row++) {
-        
-        $stepLength = $stepLengths[$i];
+        if ($currentChromaticNote == $notesInScale[$currentScaleNote]) {
+            $fretMatrix[$col][$row] = $currentScaleNote + 1;
+            $currentScaleNote++;
+            if ($currentScaleNote == sizeof($notesInScale)) {
+                $currentScaleNote = 0;
+            }
+        }
+        $currentChromaticNote++;
+        if ($currentChromaticNote > $chromaticScaleLength) {
+            $currentChromaticNote = 1;
+        }
     }
 }
 
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.col {
+    float: left;
+}
+.row {
+    font-family: 'courier';
+    text-align: center;
+    font-weight: bold;
+    border:solid 1px;
+    height:30px;
+    width:30px;
+    padding-top: 10px;
+}
+</style>
+</head>
+<body>
+
+<?php for ($col = 0; $col < $columns; $col++) : ?>
+
+<div class='col'>
+
+    <?php for ($row = 0; $row < $rows; $row++) : ?>
+
+    <div class='row'>
+        <?php if ($fretMatrix[$col][$row] != 0) {print($fretMatrix[$col][$row]);} ?>
+    </div>
+
+    <?php endfor; ?>
+
+</div>
+
+<?php endfor; ?>
+
+</body>
+</html>
+
+
