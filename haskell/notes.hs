02ba5a36a0c7cbc6cbb68540dd4e9e735b1df2c8
George Miller <geo.m.miller@gmail.com>
2016-12-28 17:51:45 -0500

    Can now get a scale of length, and also working on a better typing for scales

--- haskell/notes.hs
+++ haskell/notes.hs
@@ -4,6 +4,8 @@ data Note = A | As | B | C | Cs | D | Ds | E | F | Fs | G | Gs deriving ( Read, 
 
 data Scale = Major | Minor | Dominant deriving (Read, Show)
 
+-- data Scale (a :: Note) = Major | Minor | Dominant deriving (Read, Show)
+
 apply :: (a -> a) -> a -> Int -> a
 apply _ a 0 = a
 apply f a n = apply f (f a) (n - 1)
@@ -24,6 +26,12 @@ half = next
 generateScale :: Scale -> Note -> [Note]
 generatorHelper :: [Int] -> Note -> [Note]
 generatorHelper i n = map (apply half (prev n)) i
-generateScale Major = generatorHelper [1, 3, 5, 6, 8, 10, 12, 1]
-generateScale Minor = generatorHelper [1, 3, 4, 6, 8, 9, 11, 1]
-generateScale Dominant = generatorHelper [1, 3, 5, 6, 8, 10, 11, 1]
+generateScale Major = generatorHelper [1, 3, 5, 6, 8, 10, 12]
+generateScale Minor = generatorHelper [1, 3, 4, 6, 8, 9, 11]
+generateScale Dominant = generatorHelper [1, 3, 5, 6, 8, 10, 11]
+
+getScaleOfLength :: Scale -> Note -> Int -> [Note]
+getScaleOfLength s n i
+  | length scale >= i = take i scale
+  | otherwise = scale ++ getScaleOfLength s n (i-length scale)
+  where scale = generateScale s n
