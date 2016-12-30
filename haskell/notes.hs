fefaa620b968e308c4cd36a61f917a7978f899f4
George Miller <geo.m.miller@gmail.com>
2016-12-29 22:38:20 -0500

    Finished fretColumn! Types seem pretty awesome and fluid

--- haskell/notes.hs
+++ haskell/notes.hs
@@ -1,10 +1,11 @@
 module Notes where
 
-data Note = A | As | B | C | Cs | D | Ds | E | F | Fs | G | Gs deriving ( Read, Eq, Enum, Show )
+import Data.List (elemIndex)
+import Data.Maybe (fromMaybe)
 
-data Scale = Major | Minor | Dominant deriving (Read, Show)
+data Note = A | As | B | C | Cs | D | Ds | E | F | Fs | G | Gs deriving ( Read, Eq, Enum, Show, Ord )
 
--- data Scale (a :: Note) = Major | Minor | Dominant deriving (Read, Show)
+data Scale key = Major key | Minor key | Dominant key deriving (Read, Show)
 
 apply :: (a -> a) -> a -> Int -> a
 apply _ a 0 = a
@@ -23,15 +24,31 @@ whole = next . next
 half :: Note -> Note
 half = next
 
-generateScale :: Scale -> Note -> [Note]
+generateScale :: Scale Note -> [Note]
 generatorHelper :: [Int] -> Note -> [Note]
 generatorHelper i n = map (apply half (prev n)) i
-generateScale Major = generatorHelper [1, 3, 5, 6, 8, 10, 12]
-generateScale Minor = generatorHelper [1, 3, 4, 6, 8, 9, 11]
-generateScale Dominant = generatorHelper [1, 3, 5, 6, 8, 10, 11]
+generateScale (Major key) = generatorHelper [1, 3, 5, 6, 8, 10, 12] key
+generateScale (Minor key) = generatorHelper [1, 3, 4, 6, 8, 9, 11] key
+generateScale (Dominant key) = generatorHelper [1, 3, 5, 6, 8, 10, 11] key
 
-getScaleOfLength :: Scale -> Note -> Int -> [Note]
-getScaleOfLength s n i
+scaleOfLength :: Scale Note -> Int -> [Note]
+scaleOfLength s i
   | length scale >= i = take i scale
-  | otherwise = scale ++ getScaleOfLength s n (i-length scale)
-  where scale = generateScale s n
+  | otherwise = scale ++ scaleOfLength s (i-length scale)
+  where scale = generateScale s
+
+indexForNote :: Scale Note -> Note -> Maybe Int
+indexForNote s note = do
+  n <- elemIndex note $ generateScale s
+  Just n
+
+scaleOfLengthWithStartingNote :: Scale Note -> Note -> Int -> Maybe [Note]
+scaleOfLengthWithStartingNote s n i = do
+  start <- indexForNote s n
+  Just $ drop start $ scaleOfLength s $ i + start
+
+fretColumn :: Scale Note -> Note -> Int -> [Int]
+fretColumn s n i
+  | i == 0 = []
+  | otherwise = ( numForNote + 1 ) : fretColumn s (next n) (i-1)
+  where numForNote = fromMaybe ( -1 ) $ indexForNote s n
